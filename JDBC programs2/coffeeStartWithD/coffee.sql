3d307070cc4d1f1afacd1837318107ae2c3baae4
ITpavitra <talawarpavitra132@gmail.com>
2025-05-22 22:42:42 +0530

    Update coffee.sql

--- JDBC programs2/coffeeStartWithD/coffee.sql
+++ JDBC programs2/coffeeStartWithD/coffee.sql
@@ -1,1 +1,15 @@
+CREATE DATABASE IF NOT EXISTS test;
+
+USE test;
+
+CREATE TABLE IF NOT EXISTS coffee (
+    id INT PRIMARY KEY,
+    coffee_name VARCHAR(100),
+    price DECIMAL(10,2)
+);
+
+-- Insert some test data
+INSERT INTO coffee VALUES (1, 'Latte', 120.00);
+INSERT INTO coffee VALUES (2, 'Espresso', 80.00);
+INSERT INTO coffee VALUES (3, 'Cappuccino', 100.00);
 
